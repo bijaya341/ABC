54c7424f9b8f023853a2d21f5444ad90e994c5eb
bijaya341 <bijaya341@gmail.com>
2016-09-23 20:24:08 -0600

    added multimedia page

--- app/app.js
+++ app/app.js
@@ -7,9 +7,10 @@ window.$ = window.jQuery = $;
 require('bootstrap');
 
 // import our styles
-// import './stylesheets/base.scss';
+import './stylesheets/base.scss';
 import _ from 'underscore';
 import todoController from './pages/todo';
+// import multimediaController from './pages/multimedia';
 
 // // on document load
 $(function(){
@@ -25,8 +26,11 @@ $(function(){
 
   // kick of the app!
   // which page are we on ???
-  if (window.location.pathname === '/page/todo.html'){
+  if (window.location.pathname === '/pages/todo.html'){
     todoController.init();
+  } else if (window.location.pathname === '/pages/multimedia.html'){
+  console.log('multimedia page Started');
+
   }
   // todo.init();
 
