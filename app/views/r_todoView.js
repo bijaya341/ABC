4ea463846509c0d102aaf571d7b47e82c7697a71
bijaya341 <bijaya341@gmail.com>
2016-11-07 19:57:00 -0700

    react todo

--- app/views/r_todoView.js
+++ app/views/r_todoView.js
@@ -57,51 +57,9 @@ var todoView =React.createClass({
     if (event.which === 13){
       // create the todo
       this.createTodo(); 
-
     }
-
   }
 });
 
 module.exports = todoView;
 
-// import Backbone from 'backbone';
-// import ReactDOM from 'react-dom';
-// import React from 'react';
-// import TodoItemView from '../views/r_todoItemView';
-
-// var TodoView = Backbone.View.extend({
-//   el: '.todo-container',  //from html body main
-//   events: {
-//     'click .btn-add': 'addTodo'//,
-//     //'keypress .add-input': 'addKeypress'
-//   },
-//   initialize: function(todos, controller){
-//     this.controller = controller;
-//     this.render(todos);
-//   },
-//   render: function(todos){
-//     // render each todo item
-    
-
-//     ReactDOM.render( // react code
-//       <div>{todosHtml}</div>, 
-//       this.$el.find('.todo-list')[0] // [0] for getting vanilla from jquery
-//     );
-
-//   },
-//   removeHandlers: function(){
-//     this.$el.find('.btn-add').off();
-//     this.$el.find('.add-input').off();
-//   },
-//   addTodo: function(){
-//     var newTitle = this.$el.find('.add-input').val();
-//     this.$el.find('.add-input').val('');
-//     this.controller.addTodo(newTitle);
-//   },
-//   addKeypress: function(event){
-//     var newTitle = this.$el.find('.add-input').val();
-//     this.controller.addKeypress(event, newTitle);
-//   }
-// });
-// module.exports = TodoView;
